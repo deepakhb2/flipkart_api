368ef46b5157f0f52f41e65c2c1086962c0b6988
Girish <technopreneurG@users.noreply.github.com>
2016-02-29 03:41:44 +0530

    fixed documentation

--- lib/flipkart_delta_api.rb
+++ lib/flipkart_delta_api.rb
@@ -18,8 +18,9 @@ class FlipkartDeltaApi < FlipkartApi
   ##
   #
   # This method will get the first 500 products from the given rest api in the json parsed data structure.
+  # If version is passed only changed products since then is fetched.
   # Usage:
-  #  * fa.get_products(rest_url)
+  #  * fa.get_products(rest_url, version)
   #
   def get_products(rest_url, version=nil)
     if !rest_url.include?('fromVersion')
@@ -36,6 +37,8 @@ class FlipkartDeltaApi < FlipkartApi
   ##
   #
   # This method will get the current version of data from the delta apis
+  # Usage:
+  #  * fa.get_current_version(rest_url)
   #
   def get_current_version(rest_url)
     rest_output = RestClient.get rest_url, @header
